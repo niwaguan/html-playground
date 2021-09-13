2ccd22aad27c0570b3c096214bf8df459ebf5473
niwaguan <niwaguan@gmail.com>
2021-09-13 20:28:07 +0800

    first commit

---
+++ js/load/print-hello.js
@@ -0,0 +1,8 @@
+;(function() {
+  const max = 10000;
+  let result = "累加"
+  for (let index = 0; index < max; index++) {
+    result += index
+  }
+  console.log(result);
+})()
---
+++ js/load/print-world.js
@@ -0,0 +1,6 @@
+;(function() {
+  const max = 10000;
+  for (let index = 0; index < max; index++) {
+    console.log('world', index)
+  }
+})()
